b6e60210007e9f97dee989f94848e537a5615f84
Your Name <you@example.com>
2026-06-20 08:00:40 -0400

    fix: use relative base for pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/week2-ferdousbhuiya/",
+  base: "./",
   plugins: [react()],
 });
